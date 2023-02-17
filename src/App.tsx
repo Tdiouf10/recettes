ed3c23e5568dd53d11619e2edab4dba28a33b30e
Théodore Diouf <92777850+Tdiouf10@users.noreply.github.com>
2023-02-17 14:52:20 +0100

    Login && Signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-const App: () => JSX.Element = () => (
+import AuthProvider from "./provider/AuthProvider";
+import { Route, Routes } from 'react-router-dom'
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Recettes from "./pages/Recettes";
+import ProtectedRoute from "./routes/ProtectedRoute";
+const App = () => {
     return (
+        <AuthProvider>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="/recettes" element={
+                        <ProtectedRoute>
+                            <Recettes />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                </Route>
+            </Routes>
+        </AuthProvider>
     )
+}
 
 export default App;
