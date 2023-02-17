1519d65f21b551a2fd1f2bf3a4483a32359388ba
Eswann Gireaud <eswann@live.fr>
2023-02-17 16:11:01 +0100

    Refactor: useRecette

---
+++ src/components/hooks/useRecette.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+interface Recette {
+    idMeal: string;
+    strMeal: string;
+    strMealThumb: string;
+    strInstructions: string;
+}
+
+const ListeRecette = () => {
+    const [recettes, setRecettes] = useState<Recette[]>([]);
+
+    useEffect(() => {
+        axios
+            .get("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+            .then((response) => {
+                setRecettes(response.data.meals);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    return recettes;
+};
+
+export default ListeRecette;
--- src/pages/liste-recette/ListeRecette.tsx
+++ src/pages/liste-recette/ListeRecette.tsx
@@ -1,26 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import useRecette from '../../components/hooks/useRecette'
 
-interface Recette {
-    idMeal: string;
-    strMeal: string;
-    strMealThumb: string;
-    strInstructions: string;
-}
 
 const ListeRecette = () => {
-    const [recettes, setRecettes] = useState<Recette[]>([]);
 
-    useEffect(() => {
-        axios
-            .get("https://www.themealdb.com/api/json/v1/1/search.php?s=")
-            .then((response) => {
-                setRecettes(response.data.meals);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
+    const recettes = useRecette();
 
     return (
         <div>
