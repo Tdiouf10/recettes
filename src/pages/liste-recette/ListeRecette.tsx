7badefd716ef2c191943a8e53c2aec7257358fbb
perrineosw <oswald.perrinedu40@gmail.com>
2023-02-17 15:50:11 +0100

    recettes list

--- src/pages/liste-recette/ListeRecette.tsx
+++ src/pages/liste-recette/ListeRecette.tsx
@@ -1,13 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const ListeRecette = (): JSX.Element => {
+interface Recette {
+    idMeal: string;
+    strMeal: string;
+    strMealThumb: string;
+    strInstructions: string;
+}
 
+const ListeRecette = () => {
+    const [recettes, setRecettes] = useState<Recette[]>([]);
 
+    useEffect(() => {
+        axios
+            .get("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+            .then((response) => {
+                setRecettes(response.data.meals);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     return (
         <div>
-            Liste recette
+            <h1>Liste des recettes</h1>
+            {recettes.map((recette) => (
+                <div key={recette.idMeal}>
+                    <h2>{recette.strMeal}</h2>
+                    <img src={recette.strMealThumb} alt={recette.strMeal} />
+                    <p>{recette.strInstructions}</p>
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+};
 
-export default ListeRecette
+export default ListeRecette;
