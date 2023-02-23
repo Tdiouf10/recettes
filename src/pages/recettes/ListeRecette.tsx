9696229069b4083bc269574a1977595c6d981df2
perrineosw <oswald.perrinedu40@gmail.com>
2023-02-23 09:51:25 +0100

    style theo

--- src/pages/recettes/ListeRecette.tsx
+++ src/pages/recettes/ListeRecette.tsx
@@ -9,40 +9,25 @@ const ListeRecette = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="wrapper px-20 bg-gray-50 dark:bg-gray-900">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {recettes.map((recette) => (
-                    <div className="recette my-20 mx-10">
-                        <img src={recette.strMealThumb} alt={recette.strMeal}
-                             className="rounded-lg mx-auto block h-auto w-full lg:w-72 flex-none bg-cover h-24 border-4 border-amber-50"/>
-                        <div className="relative px-4 -mt-16">
-                            <div className="bg-white p-6 rounded-lg shadow-lg">
-                                <div className="flex items-baseline">
-                                <span
-                                    className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
-                                    {recette.strCategory}
-                                </span>
-                                    <div
-                                        className="ml-2 break-words text-gray-600 uppercase text-xs font-semibold tracking-wider">
-                                        {recette.strTags?.replace(/,/g, ", ")}
-                                    </div>
-                                </div>
-                                <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">{recette.strMeal}</h4>
-                                <div className="flex items-center justify-between">
-                                    <div className="mt-1">{recette.strArea}</div>
-                                    <button
-                                        className="bg-indigo-600 hover:bg-indigo-400 text-white font-bold py-2 px-4 rounded"
-                                        onClick={() => navigate(`/recette/${recette.idMeal}`)}>
-                                        View Recipe
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
+        // Afficher la liste des recettes sur des cartes avec tailwind css (https://tailwindcss.com/components/cards)
+        <div className="flex flex-wrap justify-center">
+            {recettes.map((recette: any) => (
+                <div
+                    className="w-full max-w-sm m-4 overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+                    <img className="object-cover w-full h-56" src={recette.strMealThumb} alt={recette.strMeal}/>
+                    <div className="p-4">
+                        <h1 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white">{recette.strMeal}</h1>
+                        <p className="text-sm text-gray-600 dark:text-gray-400">
+                            {recette.strArea}
+                        </p>
                     </div>
-                ))}
-            </div>
+                    <div className="flex items-center justify-between p-4 bg-gray-100 dark:bg-gray-700">
+                        <button className="text-sm font-medium text-indigo-500 dark:text-indigo-400 hover:underline"
+                                onClick={() => navigate(`/recette/${recette.idMeal}`)}>Voir la recette</button>
+                    </div>
+                </div>
+            ))}
         </div>
-
     );
 };
 
