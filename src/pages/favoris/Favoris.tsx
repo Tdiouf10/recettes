1b871c395aa146d94d9272ffd86ece776ab8fb91
perrineosw <oswald.perrinedu40@gmail.com>
2023-02-23 17:10:57 +0100

    save favoris

--- src/pages/favoris/Favoris.tsx
+++ src/pages/favoris/Favoris.tsx
@@ -1,4 +1,8 @@
 import useFavoris from "../../components/hooks/useFavoris"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTrash, faPencil} from "@fortawesome/free-solid-svg-icons";
+import ListModale from "../../components/ListModale";
+import React, {useState} from "react";
 
 const Favoris = (): JSX.Element => {
 
@@ -8,26 +12,48 @@ const Favoris = (): JSX.Element => {
     console.log('FavorisLists',FavorisLists)
 
     return (
-        <>
+        <div>
             <h1>
                 Favoris
             </h1>
-            <div>
+            <div className="flex flex-wrap justify-center">
                 {Object.keys(FavorisLists).map(favListName => {
                     console.log('favoris',favListName)
-                return                <div>
-                    <h2><strong>{favListName}</strong></h2>
-                    <div>
-                        {FavorisLists[favListName].map(recette =>
-                         <div>
-                            {recette.strMeal}
-                         </div>       
-                        )}
+                return (
+                    <div
+                        className="w-full max-w-sm my-10 mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+                        <div className="flex items-center p-4">
+                            <div className="flex-1">
+                                <h3 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white">{favListName}</h3>
+                            </div>
+                            <div className="mx-2">
+                                <button>
+                                    <FontAwesomeIcon
+                                        icon={faPencil}
+                                        color="green"
+                                    />
+                                </button>
+                            </div>
+                            <div className="mx-2">
+                                <button>
+                                    <FontAwesomeIcon
+                                        icon={faTrash}
+                                        color="red"
+                                    />
+                                </button>
+                            </div>
+                        </div>
+                        <div className="bg-gray-100 dark:bg-gray-700 dark:text-white p-4">
+                            {FavorisLists[favListName].map(recette =>
+                                 <div>
+                                    {recette.strMeal}
+                                 </div>
+                            )}
+                        </div>
                     </div>
-                </div>}
-                )}
+                )})}
             </div>
-        </>
+        </div>
     )
 }
 
--- src/pages/recettes/ListeRecette.tsx
+++ src/pages/recettes/ListeRecette.tsx
@@ -1,9 +1,11 @@
 import React, {useState} from "react";
 import ListModale from "../../components/ListModale";
 import useRecette from '../../components/hooks/useRecette'
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { collection, query, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
 
 
 const ListeRecette = () => {
@@ -14,7 +16,7 @@ const ListeRecette = () => {
     const [searchQuery, setSearchQuery] = useState("");
 
     const toggleFavori = (id: string) => {
-        setFavoris((prevState) => ({ ...prevState, [id]: !prevState[id] }));
+        setFavoris((prevState) => ({...prevState, [id]: !prevState[id]}));
     };
 
     const [showModal, setShowModal] = useState(false);
@@ -28,6 +30,15 @@ const ListeRecette = () => {
         recette.strCategory?.toLowerCase().includes(searchQuery.toLowerCase()) ||
         recette.strTags?.toLowerCase().includes(searchQuery.toLowerCase())
     );
+
+
+    const q = query(collection(db, "Favoris"));
+    getDocs(q)
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+            });
+        });
 
     return (
         <div className="mt-10">
@@ -45,50 +56,54 @@ const ListeRecette = () => {
             </form>
             <div className="flex flex-wrap justify-center">
                 {filteredRecettes.map((recette: any) => (
-                        <div
-                            className="w-full max-w-sm my-10 mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
-                            <img className="object-cover w-full h-56" src={recette.strMealThumb} alt={recette.strMeal}/>
-                            <div className="p-4">
-                                <div className="flex items-center">
-                                    <div className="flex-1">
-                                        <h3 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white">{recette.strMeal}</h3>
-                                    </div>
-                                    <div>
-                                        <button onClick={() => toggleFavori(recette.idMeal)}>
-                                            <FontAwesomeIcon
-                                                icon={faHeart}
-                                                color={favoris[recette.idMeal] ? "red" : "white"}
-                                                className={favoris[recette.idMeal] ? "heart--active heart-icon" : "heart--inactive heart-icon"}
-
-                                            />
-                                        </button>
-                                    </div>
+                    <div
+                        className="w-full max-w-sm my-10 mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+                        <img className="object-cover w-full h-56" src={recette.strMealThumb} alt={recette.strMeal}/>
+                        <div className="p-4">
+                            <div className="flex items-center">
+                                <div className="flex-1">
+                                    <h3 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white">{recette.strMeal}</h3>
                                 </div>
-                                <div className="flex items-baseline my-3">
-                                        <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full uppercase font-semibold tracking-wide">
-                                            {recette.strCategory}
-                                        </span>
-                                    <div className="ml-2 break-words text-gray-800 dark:text-white uppercase text-xs font-semibold tracking-wider">
-                                        {recette.strTags}
-                                    </div>
+                                <div>
+                                    <button onClick={() => toggleFavori(recette.idMeal)}>
+                                        <FontAwesomeIcon
+                                            icon={faHeart}
+                                            color={favoris[recette.idMeal] ? "red" : "white"}
+                                            className={favoris[recette.idMeal] ? "heart--active heart-icon" : "heart--inactive heart-icon"}
+
+                                        />
+                                    </button>
                                 </div>
                             </div>
-                            <div className="flex items-center bg-gray-100 dark:bg-gray-700 p-4">
-                                <div className="flex-1">
-                                    <button
-                                        className="bg-gray-100 dark:bg-white hover:text-red-600 font-bold py-2 px-4 rounded"
-                                        onClick={toggleModal}
-                                    >
-                                        <FontAwesomeIcon icon={faPlus} />
-                                    </button>
-                                    <ListModale isOpen={showModal} onClose={toggleModal}></ListModale>
-                                </div>
-                                <div className="text-right">
-                                    <button className="text-sm font-medium text-indigo-500 dark:text-indigo-600 bg-gray-100 dark:bg-white p-2 rounded hover:underline"
-                                            onClick={() => navigate(`/recette/${recette.idMeal}`)}>See more</button>
+                            <div className="flex items-baseline my-3">
+                                        <span
+                                            className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full uppercase font-semibold tracking-wide">
+                                            {recette.strCategory}
+                                        </span>
+                                <div
+                                    className="ml-2 break-words text-gray-800 dark:text-white uppercase text-xs font-semibold tracking-wider">
+                                    {recette.strTags}
                                 </div>
                             </div>
                         </div>
+                        <div className="flex items-center bg-gray-100 dark:bg-gray-700 p-4">
+                            <div className="flex-1">
+                                <button
+                                    className="bg-gray-100 dark:bg-white hover:text-red-600 font-bold py-2 px-4 rounded"
+                                    onClick={toggleModal}
+                                >
+                                    <FontAwesomeIcon icon={faPlus}/>
+                                </button>
+                                <ListModale isOpen={showModal} onClose={toggleModal}></ListModale>
+                            </div>
+                            <div className="text-right">
+                                <button
+                                    className="text-sm font-medium text-indigo-500 dark:text-indigo-600 bg-gray-100 dark:bg-white p-2 rounded hover:underline"
+                                    onClick={() => navigate(`/recette/${recette.idMeal}`)}>See more
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
